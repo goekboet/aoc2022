4e554b5d0c3b7138195326d8d8e25402b0da64ed
Erik Göök <erik.gook@hiq.se>
2023-04-06 14:04:50 +0200

    day 20 star 2

--- 20/Program.cs
+++ 20/Program.cs
@@ -2,12 +2,18 @@
 
 // long mod(long n, long m) => (n % m + m) % m;
 
-Node Fwd(Node n, int? steps = null)
+const long decryptionKey = 811589153L;
+
+Node Fwd(Node n, int l, int? steps = null)
 {
-    var i = steps ?? n.Value;
+    var i = (steps ?? n.Value) % (l - 1);
+    if (i == 0 && n.Prev is not null)
+    {
+        return n.Prev;
+    }
     while (i-- > 0)
     {
-        if (n?.Next is null) throw new Exception($"Broken link at {n?.Value}"); 
+        if (n?.Next is null) throw new Exception($"Broken link at {n?.Value}");
         n = n.Next;
     }
 
@@ -17,21 +23,25 @@ Node Fwd(Node n, int? steps = null)
 
 
 
-Node Rev(Node n)
+Node Rev(Node n, int l)
 {
-    var i = Math.Abs(n.Value);
+    var i = Math.Abs(n.Value) % (l - 1);
+    if (i == 0 && n.Next is not null)
+    {
+        return n.Next;
+    }
     while (i-- > 0)
     {
-        if (n?.Prev is null) throw new Exception($"Broken link at {n?.Value}"); 
+        if (n?.Prev is null) throw new Exception($"Broken link at {n?.Value}");
         n = n.Prev;
     }
 
     return n;
 }
 
-void InsertAfter(Node a, Node b)
+void InsertAfter(Node ptr, Node target)
 {
-    if (a == b) 
+    if (ptr == target)
     {
         throw new Exception("Trying to insert after itself.");
     }
@@ -39,68 +49,68 @@ void InsertAfter(Node a, Node b)
     // a.Prev.Next = a.Next;
     // a.Next.Prev = a.Prev;
 
-    if (b.Next is null) throw new Exception("Broken link");
-    b.Next.Prev = a;
-    a.Next = b.Next;
-    
-    a.Prev = b;
-    b.Next = a;
+    if (target.Next is null) throw new Exception("Broken link");
+    target.Next.Prev = ptr;
+    ptr.Next = target.Next;
+
+    ptr.Prev = target;
+    target.Next = ptr;
 }
 
-void InsertBefore(Node a, Node b)
+void InsertBefore(Node ptr, Node target)
 {
-    if (a == b)
+    if (ptr == target)
     {
+        Console.WriteLine(ptr.Value);
         throw new Exception("Trying to insert before itself.");
-    } 
-
-    if (b.Prev is null) throw new Exception("Broken link");
-    b.Prev.Next = a;
-    a.Prev = b.Prev;
-    
-    a.Next = b;
-    b.Prev = a;
-}
-
-int Count(Node n)
-{
-    var r = 1;
-    var ptr = n.Next;
-    while (ptr != n)
-    {
-        if (ptr is null) throw new Exception("Broken link.");
-        r++;
-        ptr = ptr.Next;
     }
 
-    return r;
+    if (target.Prev is null) throw new Exception("Broken link");
+    target.Prev.Next = ptr;
+    ptr.Prev = target.Prev;
+
+    ptr.Next = target;
+    target.Prev = ptr;
 }
 
-string Print(Node n)
-{
-    var sb = new StringBuilder();
-    sb.Append(n.Value.ToString());
-    var ptr = n.Next;
-    while (ptr != n)
-    {
-        if (ptr is null) throw new Exception("Broken link.");
-        sb.Append($", {ptr.Value}");
-        ptr = ptr.Next;
-    }
+// int Count(Node n)
+// {
+//     var r = 1;
+//     var ptr = n.Next;
+//     while (ptr != n)
+//     {
+//         if (ptr is null) throw new Exception("Broken link.");
+//         r++;
+//         ptr = ptr.Next;
+//     }
 
-    return sb.ToString();
-}
+//     return r;
+// }
+
+// string Print(Node n)
+// {
+//     var sb = new StringBuilder();
+//     sb.Append(n.Value.ToString());
+//     var ptr = n.Next;
+//     while (ptr != n)
+//     {
+//         if (ptr is null) throw new Exception("Broken link.");
+//         sb.Append($", {ptr.Value}");
+//         ptr = ptr.Next;
+//     }
+
+//     return sb.ToString();
+// }
 
 var ns = File.ReadAllLines(args[0]).Select(int.Parse).ToArray();
-var initialSet = new HashSet<int>(ns);
-var init = new Node { Value = ns[0] };
+var init = new Node { Value = ns[0] * decryptionKey };
 var current = init;
 var initialOrder = new Node[ns.Length];
 initialOrder[0] = init;
-Node? zero = null; 
+Node? zero = null;
 for (int i = 1; i < ns.Length; i++)
 {
-    var n = new Node { Value = ns[i], Prev = current };
+    var n = new Node { Value = ns[i] * decryptionKey, Prev = current };
     current.Next = n;
 
     current = n;
@@ -114,9 +124,9 @@ current.Next = init;
 init.Prev = current;
 
 Node? ptr = null;
-for (int i = 0; i < ns.Length; i++)
+for (int i = 0; i < ns.Length * 10; i++)
 {
-    ptr = initialOrder[i];
+    ptr = initialOrder[i % ns.Length];
     if (ptr.Prev is null || ptr.Next is null) throw new Exception("Broken link");
     if (ptr.Value == 0) continue;
     ptr.Prev.Next = ptr.Next;
@@ -124,43 +134,38 @@ for (int i = 0; i < ns.Length; i++)
 
     if (ptr.Value < 0)
     {
-        var target = Rev(ptr);
+        var target = Rev(ptr, ns.Length);
         InsertBefore(ptr, target);
-        
     }
     if (ptr.Value > 0)
     {
-        var target = Fwd(ptr);
+        var target = Fwd(ptr, ns.Length);
         InsertAfter(ptr, target);
     }
 }
 
 if (zero is null) throw new Exception("Did not find zero");
 
-var r = new [] {0,0,0};
+var r = new[] { 0L, 0L, 0L };
 for (int i = 0; i < r.Length; i++)
 {
     if (zero is null) throw new Exception("Broken link.");
-    zero = Fwd(zero, 1000);
+    zero = Fwd(zero, ns.Length, 1000);
     r[i] = zero.Value;
 }
 
-var finalCount = Count(zero);
+// var finalCount = Count(zero);
 
 // Console.WriteLine($"input count: {ns.Length} result count: {finalCount}");
-Console.WriteLine(Print(init));
+// Console.WriteLine(Print(init));
 Console.WriteLine(r.Sum());
-// 23239 too high
-// -17333 not correct
-// 15032 not correct
-// 8216 not correct
-// 2637 not correct
-// 12855 not correct
+// -2424216800011 not
+
 
 
 public class Node
 {
-    public int Value {get;set;}
-    public Node? Prev {get;set;} 
-    public Node? Next {get;set;}
+    public long Value { get; set; }
+    public Node? Prev { get; set; }
+    public Node? Next { get; set; }
 }
